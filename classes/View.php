954b8c9c1e8d5c094af874e8b28a71c9ba9d4d94
bilgesuyarar <bilgesuyararyb@gmail.com>
2019-02-19 13:46:25 +0300

    added classes folder

---
+++ classes/View.php
@@ -0,0 +1,47 @@
+<?php
+class View{
+	private $controller;
+	private $action;
+	private $request;
+
+  
+    //controller ve action undefined index notice diyor 11 ve 16daki bunları düzeltemedim
+	public function __construct($request){
+		$this->request = $request;
+		if($this->request['controller'] == ""){
+			$this->controller = 'home';
+		} else {
+			$this->controller = $this->request['controller'];
+		}
+		if($this->request['action'] == ""){
+			$this->action = 'index';
+		} else {
+			$this->action = $this->request['action'];
+		}
+	}
+
+	public function createController(){
+		// Check if class exists
+		if(class_exists($this->controller)){
+			$parents = class_parents($this->controller);
+			// Check Extend
+			if(in_array("Controller", $parents)){
+				if(method_exists($this->controller, $this->action)){
+					return new $this->controller($this->action, $this->request);
+				} else {
+					// Method Does Not Exist
+					echo '<h1>Method does not exist</h1>';
+					return;
+				}
+			} else {
+				// Base Controller Does Not Exist
+				echo '<h1>Main controller not found</h1>';
+				return;
+			}
+		} else {
+			// Controller Class Does Not Exist
+			echo '<h1>Controller class does not exist</h1>';
+			return;
+		}
+	}
+}
