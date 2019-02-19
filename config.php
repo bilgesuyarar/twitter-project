282f177963e4249cfeda81b01d86653ad4fb0e5b
bilgesuyarar <bilgesuyararyb@gmail.com>
2019-02-19 13:45:36 +0300

    added config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define DB Parameters
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "myproject");
+
+// Define URL
+define("ROOT_PATH", "/myproject/");
+define("ROOT_URL", "http://localhost/myproject/");
